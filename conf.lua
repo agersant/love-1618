4a9327a9bf6b53c4a6e10aa7747ec3b1b20bfbb5
Antoine Gersant <antoine.gersant@lesforges.org>
2020-08-30 15:45:09 -0700

    Disable audio to remove output noise

--- conf.lua
+++ conf.lua
@@ -1,5 +1,8 @@
 io.stdout:setvbuf("no");
 io.stderr:setvbuf("no");
 
-love.conf = function(options) options.console = true; end
+love.conf = function(options)
+    options.console = true;
+    options.modules.audio = false;
+end
 
